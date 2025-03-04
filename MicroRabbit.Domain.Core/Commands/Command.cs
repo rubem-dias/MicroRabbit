29f8bca6d7cd7b80f39f33100d1ee8f6037d53db
rubem-dias <rubemdias65@gmail.com>
2025-03-03 21:29:44 -0300

    adding command

---
+++ MicroRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,11 @@
+﻿namespace MicroRabbit.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; protected set; }
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
